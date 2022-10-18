6cff98d567f07ffef2279f0d78659fc2391bc69d
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-10-18 16:28:46 +0300

    initial commit

---
+++ mad-lib.rb
@@ -0,0 +1,14 @@
+puts "Mad Libs Game"
+
+puts "Enter color: "
+color = gets.chomp()
+
+puts "Enter plural noun: "
+plural_noun = gets.chomp()
+
+puts "Enter celebrity: "
+celebrity = gets.chomp()
+
+puts ("Roses are #{color}")
+puts ("#{plural_noun} are blue")
+puts ("I love #{celebrity}")
